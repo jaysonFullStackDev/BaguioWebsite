0388e68df4096a8ee954c7213a10e23394f8252b
jaysonFullStackDev <jayson.a.quisquirin@gmail.com>
2026-03-18 13:34:42 +0800

    transferred express in a app.js file and added configutaion in express

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.send("welcome");
 });
