6389e29b13beb626f59bfbd9e1049fc69be14202
jaysonFullStackDev <jayson.a.quisquirin@gmail.com>
2026-03-20 00:01:48 +0800

    Updated the healthcheack controller. Added constant variable into the health cheacher

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,11 +1,14 @@
 import { ApiResponse } from "../utils/api-response.js";
 
-const healthCheck = (req, res) => {
+const healthCheck = async (req, res, next) => {
   try {
+    const user = await getUserFromDB();
     res
       .status(200)
       .json(new ApiResponse(200, { message: "Server is running" }));
-  } catch (error) {}
+  } catch (error) {
+    next(err);
+  }
 };
 
 export { healthCheck };
