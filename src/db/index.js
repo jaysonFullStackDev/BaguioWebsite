51e070b59a6a62ba67ad798edc650bb2d5744bfc
jaysonFullStackDev <jayson.a.quisquirin@gmail.com>
2026-03-23 14:30:36 +0800

    Fixed the bug in the connectDB method

--- src/db/index.js
+++ src/db/index.js
@@ -2,13 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
-      socketTimeoutMS: 45000, // Close sockets after 45s of inactivity
-      tls: true,
-      tlsAllowInvalidCertificates: true,
-      tlsAllowInvalidHostnames: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection error", error);
