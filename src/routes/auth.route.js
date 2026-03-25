900c530beb197787ff79c48e662eb85adf249619
jaysonFullStackDev <jayson.a.quisquirin@gmail.com>
2026-03-25 16:14:12 +0800

    Added middleware validator to the code file

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { registeredUser } from "../controllers/auth.controller.js";
+import { validate } from "../middlewares/validator.middleware.js";
+import { userRegisterValidator } from "../validators/index.js";
 
 const router = Router();
 
